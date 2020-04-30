181e1163b89fb4788a80e1948dcf2bfee84aa311
ahmet-soylu <39171656+ahmet-soylu@users.noreply.github.com>
2020-04-30 11:03:24 +0200

    Update config.js

--- config/config.js
+++ config/config.js
@@ -7,6 +7,6 @@ var config = new Object();
 
 // SPARQL
 config.sparqlBase = "http://localhost:3030/ocds/sparql";
-config.resultBase = "http://52.19.213.234:7200/repositories/TBFY";
-config.externalBase = "http://52.19.213.234:7200/sparql";
+config.resultBase = "http://52.19.213.234:3030/tbfy/query";
+config.externalBase = "•http://52.19.213.234:3030/tbfy/query";
 config.resourceBase = "http://52.19.213.234:7200/resource"
